6676c2b3975bbf2bd060f1b4ed2ec7076837d861
SERGIO <SERGIO@LAPTOP-V9CGA352>
2021-11-25 20:29:28 -0500

    Agregar archivos de proyecto.

---
+++ TadPila/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TadPila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pila Pila = new Pila();
+            Pila.Acceso();
+        }
+    }
+}
